6e0f169067aa5df8e67bfcaddd30ff28e74785f2
Bozo4Hire <nor.guzcha@gmail.com>
2023-12-01 02:59:58 -0800

    wefwef

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -69,9 +69,13 @@ const loginUser = async (req, res) => {
         const match = await comparePassword(password, user.password)
 
         if (match) {
-            jwt.sign({ email: user.email, id: user._id, name: user.name }, process.env.JWT_SECRET, {}, (err, token) => {
+            /* jwt.sign({ email: user.email, id: user._id, name: user.name }, process.env.JWT_SECRET, {}, (err, token) => {
                 res.cookie('token', token).json(user)
+            }) */
+            return res.json({
+                error: "Las contraseñas coninciden"
             })
+            
         }
         if (!match) {
             return res.json({
